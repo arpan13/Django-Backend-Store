1688ad0ebd1ad3ac231c3754fb9300ee517e75d9
arpan13 <arpankumarpal728@gmail.com>
2021-06-05 13:29:57 +0530

    Completed category model, view, Serilization,route

---
+++ ecom/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include,path
+
+from rest_framework.authtoken import views
+
+from .views import home
+
+urlpatterns=[
+    path('',home,name="api.home"),
+    path('category/',include('api.category.urls'))
+]
